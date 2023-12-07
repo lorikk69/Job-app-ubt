ccbfb70d12c7b46db784dd0e9d316c4d6d20c44a
lorikker <loriklirak@gmail.com>
2023-12-07 19:36:27 +0100

    third commit

--- Shpalljet/Shpallje_pune.js
+++ Shpalljet/Shpallje_pune.js
@@ -3,6 +3,7 @@ function displayJobs() {
     jobList.innerHTML = ''; 
 
     const storedJobs = JSON.parse(localStorage.getItem('jobs')) || [];
+    const button = document.querySelector('.buttoni_forma');
 
     storedJobs.forEach(job => {
         const jobItem = document.createElement('li');
@@ -13,10 +14,31 @@ function displayJobs() {
             <strong>Pozita:</strong> ${job.pozita}<br>
             <strong>Përshkrimi i punës:</strong> ${job.about_us}<br>
             <strong>Paga:</strong> ${job.paga} €<br>
+            <button class="buttoni_forma">Posto punë</button>
+
             <hr>
         `;
+        jobItem.querySelector('.buttoni_forma').addEventListener('click', () => rootbutton());
         jobList.appendChild(jobItem);
     });
+
+  
+    
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    displayJobs();
+
+    const button = document.querySelector('.buttoni_forma');
+    
+    button.addEventListener('click', rootbutton);
+})
+;
+
+
+function rootbutton() {
+    window.location.href = '../Forma/forma.html';
+}
+
 
 window.onload = displayJobs;
